0690e2d15931fa71445fb7b93956e6f0c8bb78dd
Tyrell Scott <44786641+0zark@users.noreply.github.com>
2020-02-03 23:22:46 -0700

    Charactermover

---
+++ Ogre - Boulder/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Speed Attributes")]
+    public float speed = 3.5f;
+    void Update()
+    {
+        MovementCalc(); //Update on movement
+    }
+
+    private void MovementCalc()
+    {
+        //grab the input axis for horizontal and vertical movement in unity physics
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Time.deltaTime * horizontalInput * speed * Vector3.right);
+        transform.Translate(Time.deltaTime * verticalInput * speed * Vector3.up);
+
+    }
+}
