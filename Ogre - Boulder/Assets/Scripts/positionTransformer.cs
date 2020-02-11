b4f6e3d3efa692508b4857f5b84d163f53a51046
Tyrell <tyrellscott01@gmail.com>
2020-02-10 17:12:05 -0700

    Out of Bounds Created

--- Ogre - Boulder/Assets/Scripts/positionTransformer.cs
+++ Ogre - Boulder/Assets/Scripts/positionTransformer.cs
@@ -1,13 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class positionTransformer : MonoBehaviour
 {
-    public GameObject player;
+    public float areaOfEffect;
+    public Rigidbody rigidbody;
 
-    void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        player.transform.position = new Vector3(0,0,0);
+        if (transform.position.y <= areaOfEffect)
+        {
+            transform.position = new Vector3(51, 7.5f, -57.5f);
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.angularVelocity = Vector3.zero;
+        }
     }
 }
