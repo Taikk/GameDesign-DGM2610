d8d22b267d4506b854d21648802ef8dbb5fc2cd0
Tyrell Scott <44786641+0zark@users.noreply.github.com>
2020-02-28 12:03:26 -0700

    Movement Update w/ Indicator
    Changed movement to be incremental and have a indicator suggesting how strong and hard somthing is going to move

--- Ogre - Boulder/Assets/Scripts/Movement/BallPhysics.cs
+++ Ogre - Boulder/Assets/Scripts/Movement/BallPhysics.cs
@@ -1,32 +1,60 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 public class BallPhysics : MonoBehaviour
 {
-    public float ForwardForce = 0f;
+    public float ForwardForce = 0f, scale = 2;
+    public Vector3 leftRotation, rightRotation;
+    public GameObject dirArrow;
 
     void Update()
     {
         if (Input.GetKeyDown("q"))
         {
             ForwardForce += 100f;
+            scale += 1f;
+            dirArrow.GetComponent<Transform>().localScale = new Vector3(1, scale, 1);
         }
         if (Input.GetKeyDown("w"))
         {
             ForwardForce -= 100f;
+            scale -= 1f;
+            dirArrow.GetComponent<Transform>().localScale = new Vector3(1, scale, 1);
         }
         if (Input.GetKeyDown("e"))
         {
             ForwardForce += 10f;
+            scale += .1f;
+            dirArrow.GetComponent<Transform>().localScale = new Vector3(1, scale, 1);
         }
         if (Input.GetKeyDown("r"))
         {
             ForwardForce -= 10f;
+            scale -= .1f;
+            dirArrow.GetComponent<Transform>().localScale = new Vector3(1, scale, 1);
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            transform.Rotate(leftRotation);
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            transform.Rotate(rightRotation);
         }
     }
 
     private void OnMouseDown()
     {
         GetComponent<Rigidbody>().AddRelativeForce(0, 0, ForwardForce);
+        StartCoroutine(stopMovement());
     }
 
+    IEnumerator stopMovement()
+    {
+        yield return new WaitForSeconds(5);
+        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        yield return new WaitForSeconds(.01f);
+        transform.localEulerAngles = new Vector3 (0, 0, 0);
+    }
 }
