4eadde9255639bed047bee35db376fda726471b0
Tyrell <tyrellscott01@gmail.com>
2020-03-09 12:44:26 -0600

    Event Work

--- Ogre - Boulder/Assets/Scripts/MatchBehaviours/MatchIdBehaviour.cs
+++ Ogre - Boulder/Assets/Scripts/MatchBehaviours/MatchIdBehaviour.cs
@@ -2,40 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 public class MatchIdBehaviour : IdBehaviour
 {
-    [Serializable]
+    [Serializable] 
     public struct possibleMatch
     {
         public NameId nameIdObj;
-        public UnityEvent workEvent;
+        public UnityEvent enterEvent, stayEvent;
     }
     
     public List<possibleMatch> nameIdList;
     [SerializeField]
     public Dictionary<NameId, UnityEvent> nameIdDictionary;
-   
-   
+    private IdBehaviour otherBehaviourObj;
     private NameId otherIdObj;
    
     private void OnTriggerEnter(Collider other)
     {
-        var nameId = other.GetComponent<IdBehaviour>().nameIdObj;
-        if (nameId == null) return;
-      
-        otherIdObj = nameId;
-        CheckId();
+        otherBehaviourObj = other.GetComponent<IdBehaviour>();
+        if ( otherBehaviourObj == null) return;
+        
+        otherIdObj = otherBehaviourObj.nameIdObj;
+        CheckId(1);
+    }
+    
+    private void OnTriggerStay(Collider other)
+    {
+        otherBehaviourObj = other.GetComponent<IdBehaviour>();
+        if ( otherBehaviourObj == null) return;
+        
+        otherIdObj = otherBehaviourObj.nameIdObj;
+        CheckId(2);
     }
 
-    private void CheckId()
+    private void CheckId(int other)
     {
         foreach (var obj in nameIdList)
         {
             if (otherIdObj == obj.nameIdObj)
             {
-                obj.workEvent.Invoke();
+                switch (other)
+                {
+                    case 1:
+                        obj.enterEvent.Invoke();
+                        break;
+                    case 2:
+                        obj.stayEvent.Invoke();
+                        break;
+                    default:
+                        return;
+                }
             }
         }
     }
+    
 }
---
+++ Ogre - Boulder/Assets/Scripts/ScriptableObject/FloatChecker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(menuName = "Single Variables/FloatChecker")]
+public class FloatChecker : ScriptableObject
+{
+    public FloatData floatDataObj;
+
+    public UnityEvent equalEvent;
+
+    public void CheckFloatData(FloatData other)
+    {
+        if (floatDataObj.value == other.value)
+        {
+            equalEvent.Invoke();
+        }
+    }
+}
