f818d28bea0693496e29eac4d6fcdc2e476e07ab
Tyrell <tyrellscott01@gmail.com>
2020-03-27 13:06:16 -0600

    ScoreUI

---
+++ Ogre - Boulder/Assets/Scripts/UI/ScoreUI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUI : MonoBehaviour
+{
+    public FloatData scoreObj;
+    public Text scoreText;
+    public string scoreObjString;
+
+    private void Update()
+    {
+        scoreObjString = scoreObj.ToString();
+        scoreText.text = scoreObjString;
+    }
+}
