3af92ecc0a0f2485449a89a2bdf174f50128ab10
Tyrell Scott <tyrellscott01@gmail.com>
2020-04-09 13:50:01 -0600

    UI Changes and Updates

---
+++ Ogre - Boulder/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    public void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void LoadHelp()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Help");
+    }
+    
+    public void LoadWorld()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("world");
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+}
