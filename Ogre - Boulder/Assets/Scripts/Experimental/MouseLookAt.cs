7dc86411c627f13fa0735c543d0afd156ad7e954
Laptop <tyrellscott01@gmail.com>
2020-04-12 20:00:54 -0600

    Small Changes

--- Ogre - Boulder/Assets/Scripts/Experimental/MouseLookAt.cs
+++ Ogre - Boulder/Assets/Scripts/Experimental/MouseLookAt.cs
@@ -3,25 +3,23 @@ using UnityEngine;
 
 public class MouseLookAt : MonoBehaviour
 {
-    public float movement, scale, maxValue, minValue, maxScale, minScale;
+    public float movement, scale, maxValue, minValue, maxScale, minScale, forceAdd;
     public Transform dirArrow;
     public GameObject arrowRender;
-    
+
     private void Update()
     {
         ForceStrength();
-
         if (Input.GetMouseButton(0))
         {
             Debug.Log("Click");
-            GetComponent<Rigidbody>().AddRelativeForce(0f,0f,movement);
+            GetComponent<Rigidbody>().AddRelativeForce(0f, 0f, movement);
             arrowRender.GetComponent<Renderer>().enabled = false;
             StartCoroutine(StopMovement());
         }
+
         RaycastHit hit;
-
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
+        var ray = Camera.current.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit, 100))
         {
             transform.LookAt(hit.point);
@@ -33,35 +31,34 @@ public class MouseLookAt : MonoBehaviour
         if (Input.GetKey(KeyCode.Q))
         {
             Debug.Log("Q Pressed");
-            movement += 200f;
-                scale += 0.5f;
-                if (movement >= maxValue)
-                {
-                    movement = maxValue;
-                    scale = maxScale;
-                }
+            movement += forceAdd;
+            scale += 0.5f;
+            if (movement >= maxValue)
+            {
+                movement = maxValue;
+                scale = maxScale;
+            }
 
-                dirArrow.localScale = new Vector3(2, 2, scale);
+            dirArrow.localScale = new Vector3(2, 2, scale);
         }
 
         if (Input.GetKey(KeyCode.W))
         {
-            movement -= 200f;
+            movement -= forceAdd;
             scale -= 0.5f;
-
             if (movement <= minValue)
             {
                 movement = minValue;
-                scale = minValue;
+                scale = minScale;
             }
 
             dirArrow.localScale = new Vector3(2, 2, scale);
         }
     }
+
     IEnumerator StopMovement()
     {
         yield return new WaitForSeconds(4);
         arrowRender.GetComponent<Renderer>().enabled = true;
     }
-
 }
